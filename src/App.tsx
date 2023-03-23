6e12618509ade8a81610b27be937f448524e4744
spfox24 <68411909+spfox24@users.noreply.github.com>
2023-03-23 15:10:59 -0700

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Button colorScheme="blue">Button</Button>
     </div>
   );
 }
